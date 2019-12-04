16cd02bf53bfff1b70147494b1836c6e990c1f49
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-04 12:53:31 +0100

    fine applicazione

--- LanciaDado/MainWindow.xaml.cs
+++ LanciaDado/MainWindow.xaml.cs
@@ -29,23 +29,34 @@ namespace LanciaDado
         {
             txtValore.Text = "";
             lblResult.Content = "";
+            lblPunti.Content = "100";
         }
 
         private void btnRandom_Click(object sender, RoutedEventArgs e)
         {
             int numero = int.Parse(txtValore.Text);
+            int punti = int.Parse((lblPunti.Content).ToString());
             try
             {
+                if (punti <= 0)
+                    throw new Exception("Hai 0 punti. GAME OVER!");
                 if (numero > 6)
                     throw new Exception("I numeri inseribili sono da 1 a 6.");
-                int da = 1;
-                int a = 7;
                 Random random = new Random();
-                int numeroCasuale = random.Next(da, a);
+                int numeroCasuale = random.Next(1, 7);
+                dado.Source = new BitmapImage(new Uri($@"Immagini\dado{numeroCasuale}.png", UriKind.Relative));
                 if (numeroCasuale == numero)
-                    lblResult.Content = $"Il numero uscito è {numeroCasuale}. Complimenti! Hai vinto!";
+                {
+                    lblResult.Content = $"Complimenti! Hai vinto!";
+                    punti = punti + 30;
+                    lblPunti.Content = punti.ToString();
+                }
                 else
-                    lblResult.Content = $"Il numero uscito è {numeroCasuale}. Ritenta!";
+                {
+                    lblResult.Content = $"Ritenta!";
+                    punti = punti - 5;
+                    lblPunti.Content = punti.ToString();
+                }
             }
             catch (Exception ex)
             {
