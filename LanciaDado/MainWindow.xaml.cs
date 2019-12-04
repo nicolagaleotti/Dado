00d55370e149aeec5351c08dabcbb5f6f7dfe454
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-04 13:13:15 +0100

    fine

--- LanciaDado/MainWindow.xaml.cs
+++ LanciaDado/MainWindow.xaml.cs
@@ -30,25 +30,27 @@ namespace LanciaDado
             txtValore.Text = "";
             lblResult.Content = "";
             lblPunti.Content = "100";
+            dado.Source = null;
         }
 
         private void btnRandom_Click(object sender, RoutedEventArgs e)
         {
-            int numero = int.Parse(txtValore.Text);
-            int punti = int.Parse((lblPunti.Content).ToString());
             try
             {
+                string v = txtValore.Text;
+                if (v != "1" && v != "2" && v != "3" && v != "4" && v != "5" && v != "6")
+                    throw new Exception("I numeri inseribili sono da 1 a 6.");
+                int numero = int.Parse(txtValore.Text);
+                int punti = int.Parse((lblPunti.Content).ToString());
                 if (punti <= 0)
                     throw new Exception("Hai 0 punti. GAME OVER!");
-                if (numero > 6)
-                    throw new Exception("I numeri inseribili sono da 1 a 6.");
                 Random random = new Random();
                 int numeroCasuale = random.Next(1, 7);
                 dado.Source = new BitmapImage(new Uri($@"Immagini\dado{numeroCasuale}.png", UriKind.Relative));
                 if (numeroCasuale == numero)
                 {
                     lblResult.Content = $"Complimenti! Hai vinto!";
-                    punti = punti + 30;
+                    punti = punti + 15;
                     lblPunti.Content = punti.ToString();
                 }
                 else
@@ -63,6 +65,8 @@ namespace LanciaDado
                 MessageBox.Show(ex.Message);
                 txtValore.Text = "";
                 lblResult.Content = "";
+                lblPunti.Content = "100";
+                dado.Source = null;
             }
         }
     }
